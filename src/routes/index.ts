da206548340ecd139fb8968f5a1aa06c6a00d64a
Md Rubel Hosen <rubelhosen1310@gmail.com>
2026-03-27 12:41:14 +0600

    all the routes complete added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,39 @@
 import { Router } from "express";
 import { AuthRoutes } from "../module/Auth/auth.route";
+import { TutorRoutes } from "../module/Tutor/tutor.route";
 import { BookingRoutes } from "../module/booking/booking.route";
+import { AdminRoutes } from "../module/Admin/admin.route";
+import { ReviewRoutes } from "../module/Review/review.route";
 
-const router = Router();
 
-const routerManager = [
-  {
-    path: "/auth",
-    route: AuthRoutes,
-  },
-  {
-    path: "/bookings",
-    route: BookingRoutes,
-  },
-];
 
-routerManager.forEach((route) => {
-  router.use(route.path, route.route);
-});
+const router =Router();
+ 
+const routerManager =[
+    {
+        path: '/auth',
+        route: AuthRoutes
+    },
+    {  
+        path: '/tutor',
+        route: TutorRoutes
+    },
+    {
+        path:'/booking',
+        route:BookingRoutes
+    },
+    {
+        path: '/admin',
+        route: AdminRoutes
+    },
+    {
+        path: '/review',
+        route: ReviewRoutes
+    }
+]
+
+routerManager.forEach((route)=>{
+    router.use(route.path,route.route);
+})
 
 export default router;
